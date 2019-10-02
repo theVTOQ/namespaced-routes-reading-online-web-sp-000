66a97fed61e1272080ed3ab3f280783bab669a05
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-10-02 21:38:41 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :posts, only: %i[index show new create edit update]
 
   #get '/admin/stats', to: 'stats#index'
-  scope '/admin' do
+  scope '/admin', module: 'admin' do
     resources :stats, only: [:index]
   end
 
